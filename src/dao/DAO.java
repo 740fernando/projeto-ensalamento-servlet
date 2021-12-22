d9115e2a7f94b61dbe3ec425b0cfdcdc82ff9c66
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-12-22 16:21:23 -0300

    Implementado a camada DAO

---
+++ src/dao/DAO.java
@@ -0,0 +1,51 @@
+package dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+//Classe genérica- todas as classes de dao irao extender essa classe
+// é definida com generics para parametrizar o tipo de dados da classe
+public abstract class DAO<T> {
+
+	private Session session;
+	private Class<T> clazz; // classe que o dao genrencia
+	
+	protected DAO(Class<T> clazz){
+		session = HibernateUtil.getSession(); // obter uma referencia da sessao atual e atribuir ao atributo session
+		this.clazz=clazz;
+	}
+	@SuppressWarnings("unchecked")
+	public T load(Serializable id) throws DAOException{ //retorna a entidade com base no id
+		try{
+			return (T) session.load(clazz,id);
+		} catch(HibernateException e){
+			throw new DAOException(e);
+		}		
+	}
+	public void save(T obj) throws DAOException{ // salva objeto no bd
+		try{
+			session.save(obj);
+		}catch(HibernateException e ){
+			throw new DAOException (e);
+		}
+	}
+	public void delete(T obj) throws DAOException{ //exlui objeto no bd
+		try{
+		   session.delete(obj);
+		}catch(HibernateException e ){
+			throw new DAOException(e);
+		}
+	}
+	protected List<?> list (String hql) throws DAOException { //metodo list- recebe um query como parametro
+		try{
+			Query q = session.createQuery(hql);
+			return q.list();
+		}catch(HibernateException e){
+			throw new DAOException(e);
+		}
+		
+	}
+}
---
+++ src/dao/HibernateUtil.java
@@ -0,0 +1,34 @@
+package dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateUtil {
+	
+	private static SessionFactory sf;
+
+	static{ //static porque vai ser utilizado para o sistem a toda
+		Configuration cfg = new Configuration().configure();
+		sf = cfg.buildSessionFactory();
+	}
+	public static Session getSession(){
+		return sf.getCurrentSession();
+	}
+	public static void beginTransaction(){ //inicia a transacao
+		Session session = getSession();
+		session.beginTransaction();
+	}
+	public static void commitTransaction(){  // finaliza a transacao
+		Session session = getSession();
+		session.getTransaction();
+	}
+	public static void rollbackTransaction(){ // finaliza a transacao com falha e desfaz o que foi feito anteriormente
+		Session session = getSession();
+		session.getTransaction().commit();
+	}
+}
+// detalhe = o uso do sf.getCurrentSession , como eu configurei o hibernate.cfg.xml aquela propriedade para thread,
+//significa que sempre que eu chamar o sf.getCurrentSession, ele vai devolver a referencia da session atrelada
+//a thread do usuario
